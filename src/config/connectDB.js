a7db5ffe2d596805a5814fc6d2a9a8ee35ba4947
vuquochinh <83108957+vuquocchinh@users.noreply.github.com>
2023-12-01 22:30:47 +0700

    update port to host

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("defaultdb", "root", null, {
+const sequelize = new Sequelize("defaultdb", "avnadmin", AVNS_4xFjyA2BJV1KR3_aG4 -, {
     host: "bookingcare-chinh.a.aivencloud.com",
     port: 23879,
     dialect: "mysql",
